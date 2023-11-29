a14198f7b1715841e4bb1aa6b7e2ec6872b51b82
MJun111 <kmj0568@gmail.com>
2023-11-29 19:41:41 +0900

    FIX update mapping get to post in getPreSigned URL method

--- src/main/java/com/jeontongju/storage/controller/StorageController.java
+++ src/main/java/com/jeontongju/storage/controller/StorageController.java
@@ -8,6 +8,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -17,7 +18,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class StorageController {
   private final S3Service s3Service;
 
-  @GetMapping("/file/{fileName}")
+  @PostMapping("/file/{fileName}")
   public ResponseEntity<ResponseFormat<PresignedUrlResDto>> getPresignedUrl(@PathVariable String fileName) {
 
     return ResponseEntity.ok()
